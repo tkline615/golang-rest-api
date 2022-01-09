4a429206751bbeddaa62082d6ced79e306766ef1
Tom Kline <thomasgkline@yahoo.com>
2022-01-09 09:29:17 -0500

    added connectivity to firestore, introduced FindAll, Save functionality, and created Repo interface

---
+++ repository/posts-repo.go
@@ -0,0 +1,77 @@
+package repository
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"golang-rest-api/entity"
+	"google.golang.org/api/iterator"
+	"log"
+)
+
+type PostRepository interface {
+	Save(post *entity.Post) (*entity.Post, error)
+	FindAll() ([]entity.Post, error)
+}
+
+type repo struct{}
+
+func NewPostRepository() PostRepository {
+	return &repo{}
+}
+
+const (
+	projectId      string = "golang-fb"
+	collectionName string = "posts"
+)
+
+func (*repo) Save(post *entity.Post) (*entity.Post, error) {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("Failed to create a Firestore Client: %v", err)
+		return nil, err
+	}
+
+	defer client.Close()
+
+	_, _, err = client.Collection(collectionName).Add(ctx, map[string]interface{}{
+		"ID":    post.ID,
+		"Title": post.Title,
+		"Text":  post.Text,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding a new post: %v", err)
+		return nil, err
+	}
+	return post, nil
+}
+
+func (*repo) FindAll() ([]entity.Post, error) {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("Failed to create a Firestore Client: %v", err)
+		return nil, err
+	}
+	defer client.Close()
+
+	var posts []entity.Post
+	documentIterator := client.Collection(collectionName).Documents(ctx)
+	doneSymbol := iterator.Done
+	for {
+		doc, err := documentIterator.Next()
+		if err == doneSymbol {
+			break
+		} else if err != nil {
+			log.Fatalf("Failed to iterate the list of posts: %v", err)
+			return nil, err
+		}
+		post := entity.Post{
+			ID:    doc.Data()["ID"].(int64),
+			Title: doc.Data()["Title"].(string),
+			Text:  doc.Data()["Text"].(string),
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
